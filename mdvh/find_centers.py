af9d52df37820e73205f36d1071bceac50814b9d
VarunRRaj <8183489+VarunRRaj@users.noreply.github.com>
2021-11-10 19:50:59 -0800

    Cleaned bin-finding code

--- mdvh/find_centers.py
+++ mdvh/find_centers.py
@@ -4,14 +4,18 @@ import pandas as pd
 from sklearn.cluster import KMeans
 from scipy.spatial import Voronoi, voronoi_plot_2d
 
-MAX_POWER=10
-MAX_ITER=100000
+MAX_POWER=6
+MAX_ITER=10000
 
-def find_centers(hist_name, out_name, num_clusters, save_first_pow=False):
+def find_centers(hist_name, out_name, num_clusters):
     df = pd.read_csv(hist_name, delimiter=", ", engine='python')
+
+    content = df.columns.values[-1]
+    coords = df.columns.values[:-1]
+
     kmeans = [KMeans(n_clusters=num_clusters, random_state=0, max_iter=MAX_ITER) for _ in range(MAX_POWER)]
-    PhaseSpace = np.array(df.drop(['Content'], 1).astype(float))
-    BinHeights = np.array(df['Content'].astype(float))
+    PhaseSpace = np.array(df.drop([content], 1).astype(float))
+    BinHeights = np.array(df[content].astype(float))
 
     cluster_labels = [kmeans[i].fit_predict(PhaseSpace, sample_weight=BinHeights**(i)) for i in range(MAX_POWER)]
     ratios = [max_by_min(BinHeights, labelset.astype(int)) for labelset in cluster_labels]
@@ -19,22 +23,10 @@ def find_centers(hist_name, out_name, num_clusters, save_first_pow=False):
 
     print("Best Power: ", best_pow)
     print("Max/Min: ", ratios[best_pow])
-    print(ratios)
 
     centers = kmeans[best_pow].cluster_centers_
-    nice_path = path_thru_points(pd.DataFrame(centers, columns=["Q3","Q0"]))
+    nice_path = path_thru_points(pd.DataFrame(centers, columns=coords))
     pd.DataFrame(centers).iloc[nice_path].to_csv(out_name, index=False, header=False)
-
-    if save_first_pow:
-        print("First Power:")
-        print("Max/Min: ", ratios[1])
-
-        first_pow_name = "%s_first_power.csv"%out_name[:-4]
-
-        first_pow_centers = kmeans[1].cluster_centers_
-        nice_first_path = path_thru_points(pd.DataFrame(first_pow_centers, columns=["Enu", "Q2"]))
-        pd.DataFrame(first_pow_centers).iloc[nice_first_path].to_csv(first_pow_name, index=False, header=False)
-
 
 def max_by_min(zs, labels):
     num_clusters = max(labels)+1
